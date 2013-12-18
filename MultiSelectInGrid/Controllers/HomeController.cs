658d6b1b61d4fcdfc35347f2fcf1b85f96465fa1
Jeremy Pillai <daylight.ms@gmail.com>
2013-12-18 21:56:37 +0800

    Initial commit of Visual Studio project files

---
+++ MultiSelectInGrid/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiSelectInGrid.Models;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+namespace MultiSelectInGrid.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NorthwindRepository repository = new NorthwindRepository();
+
+        public ActionResult Index()
+        {
+            ViewData["territories"] = repository.Territories;
+
+            return View();
+        }
+
+        public ActionResult Read()
+        {
+            return Json(repository.Employees);
+        }
+
+        public ActionResult Update(EmployeeViewModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.UpdateEmployee(employee);
+            }
+
+            return Json(new[] { employee });
+        }
+
+        public ActionResult Create(EmployeeViewModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                employee.EmployeeID = repository.CreateEmployee(employee);
+            }
+
+            return Json(new[] { employee });
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
